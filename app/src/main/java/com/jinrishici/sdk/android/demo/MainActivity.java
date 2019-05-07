f4633d72f0dbc9697dd782dfd5dd4b4dcf00d67b
Mystery0 <mystery0dyl520@gmail.com>
2019-05-08 03:29:32 +0800

    feat: 修复Demo错误，在Demo中实现两个方式配置

--- app/src/main/java/com/jinrishici/sdk/android/demo/MainActivity.java
+++ app/src/main/java/com/jinrishici/sdk/android/demo/MainActivity.java
@@ -20,31 +20,32 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-//		JinrishiciTextView jinrishiciTextView = findViewById(R.id.jinrisiciTextView);
-//		jinrishiciTextView.setDataFormat(new JinrishiciTextView.DataFormatListener() {
-//			@Override
-//			public String set(PoetySentence poetySentence) {
-//				return "ip:" + poetySentence.getIpAddress() + "content:" + poetySentence.getData()
-//						.getContent();
-//			}
-//		});
-//		JinrishiciClient client = JinrishiciClient.getInstance().init(this);
-//		client.getOneSentenceBackground(new JinrishiciCallback() {
-//			@Override
-//			public void done(PoetySentence poetySentence) {
-//				//TODO do something
-//				Log.i(TAG, "done: " + poetySentence.getData().getContent());
-//				((TextView) (findViewById(R.id.textView))).setText(poetySentence.getData()
-//						.getContent());
-//			}
-//
-//			@Override
-//			public void error(JinrishiciRuntimeException e) {
-//				Log.w(TAG, "error: code = " + e.getCode() + " message = " + e.getMessage());
-//				//TODO do something else
-//				((TextView) (findViewById(R.id.textView))).setText(e.getMessage());
-//			}
-//		});
+		JinrishiciTextView jinrishiciTextView = findViewById(R.id.jinrisiciTextView);
+		jinrishiciTextView.setDataFormat(new JinrishiciTextView.DataFormatListener() {
+			@Override
+			public String set(PoetySentence poetySentence) {
+				return "ip:" + poetySentence.getIpAddress() + "content:" + poetySentence.getData()
+						.getContent();
+			}
+		});
+
+		JinrishiciClient client = JinrishiciClient.getInstance().init(this);
+		client.getOneSentenceBackground(new JinrishiciCallback() {
+			@Override
+			public void done(PoetySentence poetySentence) {
+				//TODO do something
+				Log.i(TAG, "done: " + poetySentence.getData().getContent());
+				((TextView) (findViewById(R.id.textView))).setText(poetySentence.getData()
+						.getContent());
+			}
+
+			@Override
+			public void error(JinrishiciRuntimeException e) {
+				Log.w(TAG, "error: code = " + e.getCode() + " message = " + e.getMessage());
+				//TODO do something else
+				((TextView) (findViewById(R.id.textView))).setText(e.getMessage());
+			}
+		});
 
 //		PoetySentence poetySentence = JinrishiciClient.getInstance().getOneSentence();
 //		Log.i(TAG, "onCreate: "+poetySentence.getData().getContent());
